548dcc1c8d690aa370535370cebabfbddec1d05f
Mitulpawar <127743192+Mitulpawar@users.noreply.github.com>
2023-03-20 17:08:02 +0530

    Update function.groovy

--- vars/function.groovy
+++ vars/function.groovy
@@ -10,3 +10,7 @@ def new_deploy(jobname,id,pathname)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${id}:/var/lib/tomcat9/webapps/${pathname}"
 }
+def new_test(jobname1)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname1}/testing.jar"
+}
