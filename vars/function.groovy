f7a5d3e7a78ce7cf46316e6a4dfeaea24dbb27c6
Mitulpawar <127743192+Mitulpawar@users.noreply.github.com>
2023-03-20 17:04:30 +0530

    Update function.groovy

--- vars/function.groovy
+++ vars/function.groovy
@@ -6,3 +6,7 @@ def new_build()
 {
   sh 'mvn package'
 }
+def new_deploy(jobname,id,pathname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${id}:/var/lib/tomcat9/webapps/${pathname}"
+}
